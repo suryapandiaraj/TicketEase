988ffcd72236a46da85cf7f9443ac28db1403d42
Surya <pandiarajsurya@gmail.com>
2024-08-04 21:24:59 +0530

    app.js

--- TicketEase-Frontend/js/app.js
+++ TicketEase-Frontend/js/app.js
@@ -1,73 +1,91 @@
-const carousel = document.querySelector('.carousel');
+const carousel = document.querySelector(".carousel");
 let sliders = [];
-
+let movies = [
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner1.webp",
+  },
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner2.webp",
+  },
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner3.webp",
+  },
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner4.webp",
+  },
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner5.webp",
+  },
+  {
+    image: "TicketEase-FrontEnd/img/banner/banner6.webp",
+  },
+];
 let slideIndex = 0; // to track current slide index.
 
 const createSlide = () => {
-    if (slideIndex >= movies.length) {
-        slideIndex = 0;
-    }
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
 
-    // creating DOM element
-    let slide = document.createElement('div');
-    let imgElement = document.createElement('img');
+  // creating DOM element
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
 
-    // attaching all elements
-    imgElement.appendChild(document.createTextNode(''));
-    slide.appendChild(imgElement);
-    carousel.appendChild(slide);
+  // attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
 
-    // setting up image
-    imgElement.src = movies[slideIndex].image;
-    slideIndex++;
+  // setting up image
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
 
-    // setting elements classname
-    slide.className = 'slider';
+  // setting elements classname
+  slide.className = "slider";
 
-    sliders.push(slide);
+  sliders.push(slide);
 
-    if (sliders.length) {
-        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${10 * (sliders.length - 2)}px)`;
-    }
-}
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      10 * (sliders.length - 2)
+    }px)`;
+  }
+};
 
 for (let i = 0; i < 3; i++) {
-    createSlide();
+  createSlide();
 }
 
 setInterval(() => {
-    createSlide();
+  createSlide();
 }, 10000);
 
 //side navigation bar
 
 jQuery(document).ready(function () {
+  $(".login, .overlay").on("click", function () {
+    $(".sidebar").removeClass("active");
+    $(".overlay").removeClass("active");
+  });
 
-    $('.login, .overlay').on('click', function () {
-        $('.sidebar').removeClass('active');
-        $('.overlay').removeClass('active');
-    });
+  $(".open-menu").on("click", function (e) {
+    e.preventDefault();
+    $(".sidebar").addClass("active");
+    $(".overlay").addClass("active");
+    // close opened sub-menus
+    $(".collapse.show").toggleClass("show");
+    $("a[aria-expanded=true]").attr("aria-expanded", "false");
+  });
 
-    $('.open-menu').on('click', function (e) {
-        e.preventDefault();
-        $('.sidebar').addClass('active');
-        $('.overlay').addClass('active');
-        // close opened sub-menus
-        $('.collapse.show').toggleClass('show');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-    /* other code */
-
+  /* other code */
 });
-
 
 // login page
 
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById("id01");
 
 // close it
 if (target == modal) {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
