6790fbade0ac0e7505d1fd2798f36aa76f02e6ea
Surya <pandiarajsurya@gmail.com>
2024-08-04 21:33:53 +0530

    app.js

--- TicketEase-Frontend/js/data.js
+++ TicketEase-Frontend/js/data.js
@@ -1,20 +1,20 @@
 let movies = [
   {
-    image: "TicketEase-FrontEnd/img/banner/banner1.webp",
+    image: "TicketEase-Frontend/img/banner/banner1.webp",
   },
   {
-    image: "TicketEase-FrontEnd/img/banner/banner2.webp",
+    image: "TicketEase-Frontend/img/banner/banner2.webp",
   },
   {
-    image: "TicketEase-FrontEnd/img/banner/banner3.webp",
+    image: "TicketEase-Frontend/img/banner/banner3.webp",
   },
   {
-    image: "TicketEase-FrontEnd/img/banner/banner4.webp",
+    image: "TicketEase-Frontend/img/banner/banner4.webp",
   },
   {
-    image: "TicketEase-FrontEnd/img/banner/banner5.webp",
+    image: "TicketEase-Frontend/img/banner/banner5.webp",
   },
   {
-    image: "TicketEase-FrontEnd/img/banner/banner6.webp",
+    image: "TicketEase-Frontend/img/banner/banner6.webp",
   },
 ];
